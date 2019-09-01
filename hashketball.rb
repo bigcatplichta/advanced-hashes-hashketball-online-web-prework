ce334406577da7ad8821a025e1828db4206dea0e
Matt Plichta <bigcatplichta@gmail.com>
2019-09-01 19:57:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,15 +123,7 @@ def num_points_scored(player)
   game_hash.each do | team, tm_info |
     tm_info.each do | key, value |
       
-      if key[player] 
       binding.pry
-        value.each do | player_stats |
-          player_stats.each do | name, (stats) | 
-            points = stats[:points] if name == player
-            
-          end
-        end
-      end
     end 
   end
 end
