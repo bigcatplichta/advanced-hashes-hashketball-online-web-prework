b8814365cf4c1cf38fc75a470615a61c6520d163
Matt Plichta <bigcatplichta@gmail.com>
2019-09-01 23:49:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -238,13 +238,14 @@ def winning_team
     tm_info.each do | key, player_list |
       if key == :players
         player_list.each do | name_hash |
-          if game_hash[:home][:players]
+          if game_hash[:home][:players].include?(name_hash)
             name_hash.each do | name, stats |
               
                 binding.pry
               
             end
           end
+        end
       end
     end
   end
